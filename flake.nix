a2357496837f8d91de3291a8ee604089de5454b8
billow <billow.fun@gmail.com>
2025-09-10 03:33:49 +0800

    add flake drv

--- flake.nix
+++ flake.nix
@@ -6,6 +6,11 @@
     flake-utils.url = "github:numtide/flake-utils";
     ocaml-overlay.url = "github:nix-ocaml/nix-overlays";
     ocaml-overlay.inputs.nixpkgs.follows = "nixpkgs";
+    b = {
+      url = "github:b1llow/my-nix-flakes";
+      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.ocaml-overlay.follows = "ocaml-overlay";
+    };
   };
 
   outputs =
@@ -14,6 +19,7 @@
       nixpkgs,
       flake-utils,
       ocaml-overlay,
+      b,
       ...
     }:
     flake-utils.lib.eachDefaultSystem (
@@ -25,27 +31,58 @@
             ocaml-overlay.overlays.default
           ];
         };
+        inherit (pkgs)
+          stdenv
+          ;
         ocaml414 = (pkgs.ocaml-ng.ocamlPackages_4_14 or pkgs.ocamlPackages_4_14);
 
+        bap-frames = pkgs.fetchFromGitHub {
+          owner = "b1llow";
+          repo = "bap-frames";
+          rev = "refs/heads/tricore-support";
+          sha256 = "sha256-1f65TEIXncDD6N54Ton/VsoNYBoxEr1h0P2HIOSzI+o=";
+        };
+
+        tracetest = pkgs.stdenv.mkDerivation {
+          pname = "rz-tracetest";
+          version = "0.1.0";
+
+          src = self;
+
+          nativeBuildInputs = with pkgs; [
+            pkg-config
+            cmake
+            protobuf_21
+            ocaml414.piqi
+          ];
+          buildInputs = with pkgs; [
+            protobuf_21
+            openssl.dev
+            b.packages.${system}.rizin
+          ];
+
+          postPatch = ''
+            cp -r ${bap-frames} ./bap-frames
+          '';
+          preConfigure = ''
+            cd rz-tracetest
+          '';
+        };
       in
       {
         devShells = {
           default = pkgs.mkShell {
-            nativeBuildInputs = with pkgs; [
-              pkg-config
-              cmake
-              protobuf_21
-              ocaml414.piqi
-            ];
-            buildInputs = with pkgs; [
-              protobuf_21
-            ];
+            inputsFrom = [ self.packages.${system}.default ];
             shellHook = ''
-              export PATH=$PWD/build:$PATH
-              export NIX_CFLAGS_COMPILE="$NIX_CFLAGS_COMPILE -g3 -O0 -fno-omit-frame-pointer"
+              export NIX_CFLAGS_COMPILE="$NIX_CFLAGS_COMPILE -g3 -fno-omit-frame-pointer"
               export CMAKE_BUILD_TYPE="Debug"
             '';
           };
+        };
+
+        packages = {
+          inherit tracetest;
+          default = tracetest;
         };
 
       }
